45137d3f9353d5234ecc69c2e5d8cd9bf709c6ff
SafooraCh <safoorach742@gnail.com>
2026-07-13 10:02:00 +0500

    final commit

--- tests/test_cleaner.py
+++ tests/test_cleaner.py
@@ -12,7 +12,7 @@ def test_load_data():
     cleaner.load_data()
 
     assert cleaner.df is not None
-    print("✅ Test Passed: Dataset Loaded Successfully!")
+    print(" Test Passed: Dataset Loaded Successfully!")
 
 
 def test_remove_duplicates():
@@ -26,7 +26,7 @@ def test_remove_duplicates():
     after = len(cleaner.df)
 
     assert after <= before
-    print("✅ Test Passed: Duplicates Removed Successfully!")
+    print(" Test Passed: Duplicates Removed Successfully!")
 
 
 test_load_data()
